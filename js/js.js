4e6540f48772dfc4a68da13d254461f05cecb2ce
Franka <floarescou@yandex.ru>
2025-08-28 14:16:11 +0300

    Это наш первый коммит

---
+++ js/js.js
@@ -0,0 +1,27 @@
+//===   lesson 1    ===
+//деление на ноль - infinity
+console.log(4/0);
+
+//невозможные математические операции (напр. умножение строки на число)
+console.log('string' * 9);
+
+//чего-то не существует
+// console.log(n1);
+
+//существующий объект без значения
+let a;
+console.log(a);
+
+// null - не объект, а отдельный тип данных
+console.log(null);
+console.log(typeof(null));
+
+let incr = 10;
+let decr = 10;
+
+console.log(incr++, decr--);
+console.log(++incr, --decr);
+
+console.log('10' == 10);
+console.log('10' === 10);
+//===   // lesson 1    ===
