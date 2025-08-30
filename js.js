e326861ce7255bdba87720d113866cf8a728bc81
Franka <floarescou@yandex.ru>
2025-08-30 23:15:03 +0300

    это наш первый коммит

---
+++ js.js
@@ -0,0 +1,37 @@
+let first = 1;
+//infinity
+console.log(4/0);
+//NaN
+console.log("string" * 9);
+
+//null
+// console.log(n1);
+
+//undefined
+let und;
+console.log(und);
+
+let obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+console.log(obj['age']);
+
+let arr = ["plum", "orange", "apple"];
+// console.log(arr[0]);
+// console.log(arr[2]);
+
+// alert('Hello');
+// console.log(confirm('Are you here?'));
+
+// let answer = +prompt('Вам есть 18?', "18");
+// console.log(typeof(answer));
+
+console.log(null);
+console.log(typeof (null));
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
